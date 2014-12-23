91958a66187ef22e6b8a2f81bf782df8b886ef36
maksadbek <a.maksadbek@gmail.com>
2014-12-24 01:42:03 +0500

    route is ready

--- route/route_test.go
+++ route/route_test.go
@@ -2,7 +2,6 @@ package route
 
 import (
 	"github.com/Maksadbek/go-ws-daemon/conf"
-	"log"
 	"strings"
 	"testing"
 )
@@ -10,11 +9,11 @@ import (
 func TestInitialize(t *testing.T) {
 	mockConf := `
 [ds]
-    [datastore.mysql]
+    [ds.mysql]
     dsn = "root:zqwW4XYLzNwN3Dsa@tcp(54.72.185.137:3306)/test"
     limit = 20
 
-    [datastore.redis]
+    [ds.redis]
     port = 6379
     chan = "orders"
     
@@ -25,11 +24,6 @@ func TestInitialize(t *testing.T) {
 
 	reader := strings.NewReader(mockConf)
 	config, err := conf.Read(reader)
-<<<<<<< HEAD
-	log.Println(config.DS.Mysql.DSN)
-=======
-	log.Println(config)
->>>>>>> bfe3682e62f1473954d564b21829b949be5cc8dc
 	if err != nil {
 		t.Error(err)
 	}
